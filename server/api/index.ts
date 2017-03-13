5a06b6ebc8e195ed83cef1c367ca73ca47c8c255
miao jing <243127395@qq.com>
2017-03-13 17:56:21 +0800

    organize api routes

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,38 +1,5 @@
 import * as Router from "koa-router"
-import * as boom from "boom";
-
-import { validator } from "../utils";
-import { AppModel } from "../model"
+import * as app from "./app";
 
 export const apiRoutes = new Router({ prefix: "/api" })
-    .post("/app", async (ctx) => {
-        let payload = ctx.request.body
-        if (!validator.onlyChars(payload.name)) throw boom.badRequest("name is invalid")
-        else {
-            ctx.body = await AppModel.create(payload.name)
-        }
-    })
-    // return a bearer token for auth. using username & password
-    /*
-    - return
-        { n: 1, nModified: 1, ok: 1 }
-    */
-    .put("/app/:id", async (ctx) => {
-        if (!ctx.params.id) throw boom.badRequest("id_is_required")
-        let payload = ctx.request.body;
-        let option = await AppModel.repo.findById(ctx.params.id)
-        if (option.isEmpty()) throw boom.badRequest("no_entity_found")
-        await AppModel.repo.update(ctx.params.id, payload)
-        ctx.status = 200;
-    })
-    .delete("/app/:id", async ctx => {
-        if (!ctx.params.id) throw boom.badRequest("id_is_required")
-        await AppModel.repo.delete(ctx.params.id)
-        ctx.status = 200;
-    })
-    .get("/app/:id", async ctx => {
-        if (!ctx.params.id) throw boom.badRequest("id_is_required")
-        let option = await AppModel.repo.findById(ctx.params.id);
-        if (option.isEmpty()) throw boom.badRequest("no_entity_found")
-        else ctx.body = option.get()
-    })
+apiRoutes.use(app.routes.routes())
